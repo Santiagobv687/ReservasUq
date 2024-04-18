282bac6688cc7bb399c84aa8cf1e350902f78c1f
Diego Nicolás Peña Rincon; Escritorio. <diegonicolaspenarincon@gmail.com>
2024-04-18 10:58:12 -0500

    Update EditarUsuarioViewController.java

--- src/main/java/co/edu/uniquindio/reserva/reservauq/viewController/EditarUsuarioViewController.java
+++ src/main/java/co/edu/uniquindio/reserva/reservauq/viewController/EditarUsuarioViewController.java
@@ -1,4 +1,18 @@
 package co.edu.uniquindio.reserva.reservauq.viewController;
 
+import co.edu.uniquindio.reserva.reservauq.controller.InicioSesionController;
+import co.edu.uniquindio.reserva.reservauq.model.Usuario;
+import javafx.fxml.FXML;
+
 public class EditarUsuarioViewController {
+
+    Usuario usuarioIniciado;
+
+    @FXML
+
+    void initialize() {
+        usuarioIniciado = new Usuario();
+    }
+
+
 }
