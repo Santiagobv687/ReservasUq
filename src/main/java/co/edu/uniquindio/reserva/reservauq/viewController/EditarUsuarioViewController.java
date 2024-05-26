6c524dc319abf7ae9c2f8af7ebf0a2d554425fdf
Diego Nicolás Peña Rincon; Escritorio. <diegonicolaspenarincon@gmail.com>
2024-05-26 01:10:35 -0500

    Revert "Problemas con la rama main"
    This reverts commit c5725e9b495c94110829e45cf4450ecf20018356.

--- src/main/java/co/edu/uniquindio/reserva/reservauq/viewController/EditarUsuarioViewController.java
+++ src/main/java/co/edu/uniquindio/reserva/reservauq/viewController/EditarUsuarioViewController.java
@@ -9,5 +9,38 @@ import javafx.scene.control.TextField;
 
 public class EditarUsuarioViewController {
 
+    public TextField txtContrasenia;
+    private UsuarioDto usuarioIniciado;
 
+    @FXML
+    void initialize() {
+        System.out.println(usuarioIniciado);
+    }
+
+    @FXML
+    private Button btnActualizar;
+
+    @FXML
+    private TextField txtCorreo;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtNombre;
+
+
+    @FXML
+    public void actualizarUsuarioEvent(ActionEvent actionEvent) {
+        String nombreNuevo=txtNombre.getText();
+    }
+
+    @FXML
+    public void eliminarUsuarioEvent(ActionEvent actionEvent) {
+
+    }
+
+    public void setUsuarioIniciado(UsuarioDto usuarioIniciado) {
+        this.usuarioIniciado = usuarioIniciado;
+    }
 }
