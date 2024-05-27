32c607c4ddc521bbe5fb669d89386c90ec4f6ca0
Diego Nicolás Peña Rincon; Escritorio. <diegonicolaspenarincon@gmail.com>
2024-05-26 19:11:22 -0500

    aaaaa

--- src/main/java/co/edu/uniquindio/reserva/reservauq/config/RabbitFactory.java
+++ src/main/java/co/edu/uniquindio/reserva/reservauq/config/RabbitFactory.java
@@ -9,8 +9,8 @@ public class RabbitFactory {
         this.connectionFactory = new ConnectionFactory();
         this.connectionFactory.setHost("localhost");
         this.connectionFactory.setPort(5672);
-        this.connectionFactory.setUsername("guest");
-        this.connectionFactory.setPassword("guest");
+        this.connectionFactory.setUsername("Nicolas");
+        this.connectionFactory.setPassword("ElBacano");
     }
 
     public ConnectionFactory getConnectionFactory() {
