367bbd355a7f4df701d9a62076484f28fd421ac6
Santiagobv687 <santiagobv687@gmail.com>
2024-05-27 11:04:17 -0500

    x

--- src/main/java/co/edu/uniquindio/reserva/reservauq/controller/ReservaController.java
+++ src/main/java/co/edu/uniquindio/reserva/reservauq/controller/ReservaController.java
@@ -3,7 +3,10 @@ package co.edu.uniquindio.reserva.reservauq.controller;
 import co.edu.uniquindio.reserva.reservauq.mapping.dto.EventoDto;
 import co.edu.uniquindio.reserva.reservauq.mapping.dto.ReservaDto;
 import co.edu.uniquindio.reserva.reservauq.mapping.dto.UsuarioDto;
+import co.edu.uniquindio.reserva.reservauq.model.Reserva;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReservaController {
@@ -26,15 +29,31 @@ public class ReservaController {
         return modelFactoryController.obtenerEventos();
     }
 
-    public boolean agregarReserva(ReservaDto reservaDto) {
-        return modelFactoryController.agregarReserva(reservaDto);
+    public boolean agregarReserva(ReservaDto reservaDto) throws IOException {
+        boolean sePuedeAgregar=modelFactoryController.agregarReserva(reservaDto);
+        if(sePuedeAgregar)
+        {
+            modelFactoryController.producirObjeto(modelFactoryController.FILA, reservaDto);
+        }
+        return sePuedeAgregar;
     }
 
-    public boolean actualizarReserva(String idActual, ReservaDto reservaDto) {
-        return modelFactoryController.actualizarReserva(idActual, reservaDto);
+    public boolean actualizarReserva(String idActual, ReservaDto reservaDto) throws IOException {
+        boolean sePuedeActualizar=modelFactoryController.actualizarReserva(idActual, reservaDto);;
+        if(sePuedeActualizar)
+        {
+            modelFactoryController.producirObjeto(modelFactoryController.FILA, reservaDto);
+        }
+        return sePuedeActualizar;
     }
 
     public boolean eliminarReserva(String IDReserva) {
         return modelFactoryController.eliminarReserva(IDReserva);
     }
+
+    public ArrayList<ReservaDto> actualizarMensajesReservas() {
+        modelFactoryController.consumirMensajesServicio1();
+        return modelFactoryController.reservas;
+    }
+
 }
